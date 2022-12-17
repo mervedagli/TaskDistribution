b80c0606d151fcf2fa65babc6fc73f119dfd2610
mervedaqli@gmail.com <mervedaqli@gmail.com>
2022-12-18 02:45:59 +0300

    A listing was made on the task and task difficult type pages. Continuing create for task

---
+++ TaskDistribution/Controllers/TaskController.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using TaskDistribution.Repositories;
+
+namespace TaskDistribution.Controllers
+{
+    public class TaskController : Controller
+    {
+        private ITaskRepository _taskRepository;
+
+        public TaskController(ITaskRepository taskRepository)
+        {
+            _taskRepository = taskRepository;
+        }
+
+        [Route("/Task/Index")]
+        public async Task<IActionResult> Index()
+        {
+            var tasks=await _taskRepository.TList("TaskDifficultType","User");
+            return View(tasks);
+        }
+        [HttpGet]
+        public async Task<IActionResult> TaskAdd()
+        {
+            return View();
+        }
+        [HttpPost]
+        public async Task<IActionResult> TaskAdd(TaskDistribution.Data.Modals.Task task)
+        {
+            await _taskRepository.Add(task);
+            return RedirectToAction("Index");  
+        }
+    }
+}
