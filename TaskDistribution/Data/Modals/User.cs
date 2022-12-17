2f9ab1d08c37f3ee4f94d21a8371a6225b5e954c
mervedaqli@gmail.com <mervedaqli@gmail.com>
2022-12-17 20:27:16 +0300

    creation of all necessary tables for the project

---
+++ TaskDistribution/Data/Modals/User.cs
@@ -0,0 +1,13 @@
+﻿namespace TaskDistribution.Data.Modals
+{
+    public class User
+    {
+        public int UserId { get; set; } 
+        public string? User_NM { get; set; }
+        public string? Password_TXT { get; set; }
+        public int UserRoleID { get; set; }
+        public virtual UserRole? UserRole { get; set; }
+        public bool IsDeleted_FL { get; set; }
+        public List<Task>? Tasks { get; set; }   
+    }
+}
