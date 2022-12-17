247e8ba8b5569de2030a538f399b23dde44f89d3
mervedaqli@gmail.com <mervedaqli@gmail.com>
2022-12-18 02:53:20 +0300

    The task difficult type and user columns in the task table are set to nullable.

---
+++ TaskDistribution/Migrations/20221217235216_makeColumnsNullable.cs
@@ -0,0 +1,100 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace TaskDistribution.Migrations
+{
+    /// <inheritdoc />
+    public partial class makeColumnsNullable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_TaskDifficultTypes_TaskDifficultTypeID",
+                table: "Tasks");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Users_UserID",
+                table: "Tasks");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserID",
+                table: "Tasks",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TaskDifficultTypeID",
+                table: "Tasks",
+                type: "int",
+                nullable: true,
+                oldClrType: typeof(int),
+                oldType: "int");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_TaskDifficultTypes_TaskDifficultTypeID",
+                table: "Tasks",
+                column: "TaskDifficultTypeID",
+                principalTable: "TaskDifficultTypes",
+                principalColumn: "TaskDifficultTypeID");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Users_UserID",
+                table: "Tasks",
+                column: "UserID",
+                principalTable: "Users",
+                principalColumn: "UserId");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_TaskDifficultTypes_TaskDifficultTypeID",
+                table: "Tasks");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Tasks_Users_UserID",
+                table: "Tasks");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "UserID",
+                table: "Tasks",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AlterColumn<int>(
+                name: "TaskDifficultTypeID",
+                table: "Tasks",
+                type: "int",
+                nullable: false,
+                defaultValue: 0,
+                oldClrType: typeof(int),
+                oldType: "int",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_TaskDifficultTypes_TaskDifficultTypeID",
+                table: "Tasks",
+                column: "TaskDifficultTypeID",
+                principalTable: "TaskDifficultTypes",
+                principalColumn: "TaskDifficultTypeID",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Tasks_Users_UserID",
+                table: "Tasks",
+                column: "UserID",
+                principalTable: "Users",
+                principalColumn: "UserId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
